e25ccc8f5acbc78e46da044aeb68143099013984
MOwais634 <owais.jaweed5@gmail.com>
2024-09-15 03:29:15 +0500

    first commit

---
+++ lib/src/api/api_keys.default.dart
@@ -0,0 +1,10 @@
+/// To get an API key, sign up here:
+/// https://home.openweathermap.org/users/sign_up
+///
+/// Then paste it in here and rename this file to `api_keys.dart`.
+///
+/// You can also specify an API Key via --dart-define. Example:
+/// "flutter run --dart-define API_KEY=YOUR_API_KEY
+class APIKeys {
+  static const openWeatherAPIKey = '2a5d7e7a3561aed3a28690c962e5b210';
+}
---
+++ lib/src/features/weather/presentation/city_search_box.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:open_weather_example_flutter/src/constants/app_colors.dart';
+import 'package:open_weather_example_flutter/src/features/weather/application/providers.dart';
+
+class CitySearchBox extends ConsumerStatefulWidget {
+  const CitySearchBox({super.key});
+  @override
+  ConsumerState<CitySearchBox> createState() => _CitySearchRowState();
+}
+
+class _CitySearchRowState extends ConsumerState<CitySearchBox> {
+
+  late final _searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController.text = "Pakistan";
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
+  // circular radius
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: SizedBox(
+        height: 60,
+        child: Row(
+          children: [
+            Expanded(
+              child: TextField(
+                controller: _searchController,
+                textAlign: TextAlign.center,
+                style: const TextStyle(color: Colors.black),
+                decoration: const InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      bottomLeft: Radius.circular(30),
+                    ),
+                  ),
+                ),
+                // onSubmitted: (value) =>
+                //     ref.read(cityProvider.notifier).state = value,
+              ),
+            ),
+            GestureDetector(
+                 onTap: () {
+                FocusScope.of(context).unfocus();
+                ref.read(cityProvider.notifier).state = _searchController.text;
+              },
+              child: Container(
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: AppColors.accentColor,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    bottomRight: Radius.circular(30),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: Text('search',
+                      style: Theme.of(context).textTheme.bodyLarge),
+                ),
+              ),
+           
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
